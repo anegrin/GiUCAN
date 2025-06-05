cc87014f80b39a8a65c60b9b71aa0027c6afcded
anegrin <alessandro.negrin@gmail.com>
2025-06-05 16:15:11 +0200

    optional user config

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@ rm -rf dist
 rm -rf build
 mkdir -p dist
 mkdir -p build
-CFLAGS="-DSLCAN -DINCLUDE_USER_CONFIG_H" make clean all;mv build/GiUCAN*.elf dist/GiUCAN_SLCAN.elf
-CFLAGS="-DC1CAN -DINCLUDE_USER_CONFIG_H" make clean all;mv build/GiUCAN*.elf dist/GiUCAN_C1CAN.elf
-CFLAGS="-DBHCAN -DINCLUDE_USER_CONFIG_H" make clean all;mv build/GiUCAN*.elf dist/GiUCAN_BHCAN.elf
+
+EXTRA_FLAG=""
+[ -f inc/user_config.h ] && EXTRA_FLAG="-DINCLUDE_USER_CONFIG_H"
+
+CFLAGS="-DSLCAN $EXTRA_FLAG" make clean all;mv build/GiUCAN*.elf dist/GiUCAN_SLCAN.elf
+CFLAGS="-DC1CAN $EXTRA_FLAG" make clean all;mv build/GiUCAN*.elf dist/GiUCAN_C1CAN.elf
+CFLAGS="-DBHCAN $EXTRA_FLAG" make clean all;mv build/GiUCAN*.elf dist/GiUCAN_BHCAN.elf
